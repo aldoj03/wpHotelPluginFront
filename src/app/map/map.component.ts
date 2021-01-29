1e84e30cac6fb8d1ce60e77b0302d3c348fc1609
aldo <aldoj03@gmail.com>
2021-01-29 19:32:50 -0400

    "

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -9,27 +9,44 @@ import { MapboxService } from '../services/mapbox.service';
 export class MapComponent implements OnInit {
 
   @Output() setShowMap = new EventEmitter
-  @Input() center 
+  @Input() center
   @Input() markers = []
-
+  public popUps = []
+  public markersMap = []
+  public popUpMap = []
   constructor(
     private mapService: MapboxService
   ) { }
 
   ngOnInit(): void {
     console.log(this.center);
-    
+
     this.mapService.buildMap(this.center)
-    this.newMarker()
+    this.setMarkers()
   }
 
-  closeMap(){
+  closeMap() {
     this.setShowMap.emit(true)
   }
 
-  newMarker(){
-   this.markers.forEach(cords=>{
-    this.mapService.addMarker({lat:cords.lat, lng:cords.lng})
-   }) 
+  setMarkers() {
+
+     this.markers.forEach(marker => {
+      const cords = {
+        lat: marker.cords.lat,
+        lng: marker.cords.lng
+      }
+      
+     this.markersMap.push(this.mapService.buildMarker(cords, marker.text)) 
+     this.popUpMap.push(this.mapService.buildPopUp( marker.text))
+    })
+    
+
+    this.markersMap.forEach((marker,i) => {
+       this.mapService.addMarker(marker)
+       this.mapService.addPopUp(marker,this.popUpMap[i])
+    })
   }
+
+
 }
--- src/app/services/mapbox.service.ts
+++ src/app/services/mapbox.service.ts
@@ -11,18 +11,20 @@ export class MapboxService {
   public mapbox = (mapboxgl as typeof mapboxgl);
   public map: mapboxgl.Map;
   public style = `mapbox://styles/mapbox/streets-v11`;
-  public lat ;
-  public lng ;
+  public lat;
+  public lng;
   public zoom = 15;
+  public marker: mapboxgl.Marker;
+  public popup: mapboxgl.Popup;
 
   constructor() {
     this.mapbox.accessToken = environment.mapBoxToken
-   }
+  }
 
 
-   buildMap(center) {
-     this.lat = center.lat
-     this.lng = center.lng
+  buildMap(center) {
+    this.lat = center.lat
+    this.lng = center.lng
     this.map = new mapboxgl.Map({
       container: 'map',
       style: this.style,
@@ -30,17 +32,42 @@ export class MapboxService {
       center: [this.lng, this.lat]
     });
     // this.map.addControl(new mapboxgl.NavigationControl());
-    }
+  }
 
-    addMarker(cords){
-      
-  // create a HTML element for each feature
-  var el = document.createElement('div');
-  el.className = 'marker';
+  addMarker(marker) {
 
 
+    marker.addTo(this.map)
+    // this.marker.setPopup(this.popup) 
+    // this.marker.getElement().addEventListener('mouseenter',this.togglePopup)
+    // this.marker.getElement().addEventListener('mouseleave',this.togglePopup)
+  }
 
-      const maker = new mapboxgl.Marker(el).setLngLat({lat:cords.lat, lng: cords.lng})
-      maker.addTo(this.map)
-    }
+  togglePopup() {
+    this.marker.togglePopup()
+
+  }
+
+  buildMarker(cords, text) {
+
+    const el = document.createElement('div');
+    el.innerHTML = Number(text).toFixed(2)
+    el.classList.add('custom_marker');
+    const marker = new mapboxgl.Marker(el)
+    marker.setLngLat({ lat: cords.lat, lng: cords.lng })
+
+    return marker
+  }
+
+  buildPopUp(text) {
+    const popUp = new mapboxgl.Popup({ offset: 25 })
+    popUp.setHTML('<h3>' + text + '</h3><p></p>')
+  }
+
+
+  addPopUp(marker, popUp) {
+    marker.setPopup(popUp)
+  }
+
+
 }
